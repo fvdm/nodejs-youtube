50f46e5cdf450d55743f61a0025b675959303e06
Franklin <fr@nkl.in>
2012-06-18 15:02:08 +0200

    Framework and API talk

---
+++ youtube.js
@@ -0,0 +1,95 @@
+var	https = require('https'),
+	EventEmitter = require('events').EventEmitter,
+	querystring = require('querystring')
+
+var app = new EventEmitter()
+
+
+///////////
+// FEEDS //
+///////////
+
+app.feed = {
+	
+}
+
+
+/////////////////
+// COMMUNICATE //
+/////////////////
+
+app.talk = function( type, path, fields, cb ) {
+	
+	// fix callback
+	if( cb == undefined && typeof fields == 'function' ) {
+		var cb = fields
+		var fields = {}
+	}
+	
+	// fix fields
+	if( fields == undefined || typeof fields != 'object' ) {
+		var fields = {}
+	}
+	
+	// force JSON
+	fields.alt = 'json'
+	
+	// prepare
+	var requestHeaders = {
+		'User-Agent':	'youtube-api.js (https://github.com/fvdm/nodejs-youtube)',
+		'Accept':		'application/json'
+	}
+	
+	var query = querystring.stringify( fields )
+	
+	doPost = false
+	if( type.match( /(POST|DELETE|PUT)/ ) ) {
+		doPost = true
+		requestHeaders['Content-Type'] = 'applications/x-www-form-urlencoded'
+		requestHeaders['Content-Length'] = query.length
+	} else {
+		path += '?'+ query
+	}
+	
+	var options = {
+		hostname:		'gdata.youtube.com',
+		port:			443,
+		path:			'/'+ path,
+		headers:		requestHeaders,
+		method:			type,
+		agent:			false
+	}
+	
+	// request
+	var request = https.request( options, function( response ) {
+		
+		// response
+		response.setEncoding('utf8')
+		var data = ''
+		
+		response.on( 'data', function( chunk ) { data += chunk })
+		response.on( 'end', function() {
+			
+			// validate
+			if( data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -1, 1 ) == '}' ) {
+				
+				// done
+				cb( JSON.parse( data ) )
+				
+			}
+			
+		})
+		
+	})
+	
+	// post & do it
+	if( doPost ) {
+		request.send( query )
+	}
+	
+	request.end()
+	
+}
+
+// ready
+module.exports = app
