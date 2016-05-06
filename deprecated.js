512f17d4c972ac126756696dfa3965a1fc2c9714
Franklin van de Meent <fr@nkl.in>
2016-05-06 04:19:58 +0200

    Added deprecated.js for npm pre-install

---
+++ deprecated.js
@@ -0,0 +1,2 @@
+console.log ('\u001b[31mDEPRECATED\u001b[0m - youtube-feeds is deprecated, use googleapis instead');
+console.log ('\u001b[31mDEPRECATED\u001b[0m - Example at https://github.com/fvdm/nodejs-youtube/wiki');
